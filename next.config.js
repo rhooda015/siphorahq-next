ab0dc582248232f03427879ecfc5f76f50b880ca
rhooda015 <88473858+rhooda015@users.noreply.github.com>
2026-06-07 23:24:04 +0530

    fix: disable image optimization to fix 400 errors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     root: __dirname,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -18,10 +19,6 @@ const nextConfig = {
         hostname: 'siphorahq-backend-production.up.railway.app',
       },
     ],
-    formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 60 * 60 * 24 * 30,
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256],
   },
   compress: true,
 };
